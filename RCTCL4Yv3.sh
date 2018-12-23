dd9bfd4b3364140a360597b38bd03c77c006ffc9
RabbitCL4Y <44805762+RabbitCL4Y@users.noreply.github.com>
2018-12-23 21:09:23 +0800

    Create RCTCL4Yv3.sh

---
+++ RCTCL4Yv3.sh
@@ -0,0 +1,857 @@
+﻿#Kalo mau recode izin dulu sama author
+#Bisa hubungi lewat Email: RabbitCL4Y@gmail.com
+#Author: MR.CL4Y
+#!/system/xbin/bash
+green="\033[32;1m"
+yellow="\033[33;1m"
+indigo="\033[34;1m"
+ungu="\033[35;1m"
+cyan="\033[36;1m"
+white="\033[37;1m"
+red="\033[31;1m"
+clear
+echo
+echo
+echo
+echo
+echo $red"        TUNGGU TOOLS SELESAI DI INSTALL DALAM WAKTU 5 DETIK"
+echo $red"                     HARAP SABAR MENUNGGU :)"   
+sleep 1
+echo '''\a
+\033[34;1m                              _
+\033[34;1m                             / |
+\033[34;1m                             | |
+\033[34;1m                             | |
+\033[34;1m                             |_| '''
+sleep 1
+clear
+echo
+echo
+echo
+echo
+echo $red"        TUNGGU TOOLS SELESAI DI INSTALL DALAM WAKTU 5 DETIK"
+echo $red"                     HARAP SABAR MENUNGGU :)"
+sleep 1
+echo '''\a
+\033[32;1m                             ____
+\033[32;1m                            |___ \
+\033[32;1m                             __) |
+\033[32;1m                            / __/
+\033[32;1m                           |_____| '''
+sleep 1
+clear
+echo
+echo
+echo
+echo
+echo $red"        TUNGGU TOOLS SELESAI DI INSTALL DALAM WAKTU 5 DETIK"
+echo $red"                     HARAP SABAR MENUNGGU :)"
+sleep 1
+echo '''\a
+\033[35;1m                            _____
+\033[35;1m                           |___ /
+\033[35;1m                             |_ \
+\033[35;1m                            ___) |
+\033[35;1m                           |____/ '''
+sleep 1
+clear
+echo
+echo
+echo
+echo
+echo $red"        TUNGGU TOOLS SELESAI DI INSTALL DALAM WAKTU 5 DETIK"
+echo $red"                     HARAP SABAR MENUNGGU :)"
+sleep 1
+echo '''\a
+\033[33;1m                            _  _
+\033[33;1m                           | || |
+\033[33;1m                           | || |_
+\033[33;1m                           |__   _|
+\033[33;1m                              |_| '''
+sleep 1
+clear
+echo
+echo
+echo
+echo
+echo $red"        TUNGGU TOOLS SELESAI DI INSTALL DALAM WAKTU 5 DETIK"
+echo $red"                     HARAP SABAR MENUNGGU :)"
+sleep 1
+echo '''\a
+\033[36;1m                             ____
+\033[36;1m                            | ___|
+\033[36;1m                            |___ \
+\033[36;1m                             ___) |
+\033[36;1m                            |____/ '''
+sleep 1
+clear
+echo
+echo $red
+pkg install toilet -y
+clear
+echo $green
+pkg install figlet -y
+clear
+echo $cyan
+pkg install ruby -y
+clear
+gem install figlet
+clear
+gem install lolcat
+clear
+pkg install curl
+clear
+echo
+echo
+echo
+echo
+echo
+echo
+sleep 1
+toilet -f future "    Install Bahan Selesai" | lolcat
+sleep 5
+clear
+echo
+echo
+echo
+echo
+echo
+echo
+toilet -f future " Wellcome to my tools" | lolcat
+sleep 5
+clear
+sh 77
+# Bersihkan Layar
+clear
+blue='\033[34;1m'
+green='\033[32;1m'
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'
+yellow='\033[33;1m'
+###################################################
+# CTRL C-
+###################################################
+trap ctrl_c INT
+ctrl_c() {
+clear
+echo  $red"[#]> (Ctrl + C ) Detected, 
+Trying To Exit ... "
+sleep 1
+echo ""
+echo  $yellow"[#]> Thank You For Using My 
+Tools ... "
+sleep 1
+echo ""
+echo  $white"[#]> Rabbit Cyber Team Was 
+Here ... "
+sleep 10
+read enter
+exit
+}
+=================================================
+echo $yellow
+figlet -f smshadow "TOOLS MR.CL4Y"
+echo $indigo
+date
+echo $purple"___________________________________________________"
+echo $cyan" ["$red"+"$cyan"]Author by	: MR.CL4Y "
+echo $cyan" ["$red"+"$cyan"]Team        : Rabbit Cyber Team		"
+echo $cyan" ["$red"+"$cyan"]Github	: https://github.com/RabbitCL4Y"
+echo $cyan" ["$red"+"$cyan"]Whatsapp : 085752481601"
+echo $cyan" ["$red"+"$cyan"]Instagram : muhammadrafli_337"
+echo $cyan" ["$red"+"$cyan"]Facebook : Rafli Mhmmd"
+echo $purple"___________________________________________________"
+echo $cyan" 1) install DDoS MR.CL4Y"
+echo $green" 2) install Brute-Webdav-MR.CL4Y"
+echo $yellow" 3) install tools installer v1"
+echo $purple" 4) install tools installer v2"
+echo $red" 5) install tools installer v3"
+echo $white" 6) install tools RCT"
+echo $blue" 7) install tools TUANB4DUT"
+echo $green" 8) install tools Cyb3r_Gh0st"
+echo $purple" 9) install DarkFly-Tool"
+echo $yellow" 10) install tools MR.TR3V!0N"
+echo $blue" 11) install Tools Bajinganv6"
+echo $purple" 12) install Recondog"
+echo $cyan" 13) install sqlmap"
+echo $red" 14) install OSIF"
+echo $green" 15) install MBF"
+echo $yellow" 16) FCTSPAMMER"
+echo $white" 17) tools kenangan"
+echo $purple" 18) Mr.Errorv1"
+echo $yellow" 19) AdminFinder"
+echo $green" 20) biodata MR.CL4Y"
+echo $red" 21) install tools Installerv4"
+echo $white" 22) Tembak-Tembakan"
+echo $blue" 23) install D-Tect"
+echo $yellow" 24) installing Weeman"
+echo $lightgreen" 25) Install Katolin"
+echo $green" 26) install IPGeolocation"
+echo $purple" 27) Termux Ohmyzsh"
+echo $white" 28) install ipcs"
+echo $cyan" 29) Santet-Mantan"
+echo $lightgreen" 30) Install mrcakil"
+echo $white" 31) install Joomscan"
+echo $yellow" 32) install shell-scan"
+echo $blue" 33) install Black-Hydra"
+echo $purple" 34) install BingOO"
+echo $cyan" 35) install ShellPish"
+echo $lightgreen" 36) install LITESCRIPT"
+echo $red" 37) install TU4NN1CKN4M3"
+echo $green" 38) install diejoubu"
+echo $blue" 39) install websploit"
+echo $lightgreen" 40) install Ko-Dork"
+echo $blue" 41) install tools Z3X"
+echo $lightgreen" 42) install InYours3rv3r"
+echo $cyan" 43) install Malicious"
+echo $blue" 44) install Matematika"
+echo $yellow" 45) install Lazymux"
+echo $red" 46) install Tool-X"
+echo $purple" 47) install Kubik-BOT"
+echo $white" 48) install PhisingGame"
+echo $yellow" 49) install BotKomena"
+echo $purple" 50) tembak cewe supaya gak kena tolak"
+echo  $red" 0) Exit "
+echo  $white""
+read -p "[root@MRCL4Y]> " bos;
+
+if [ $bos = 1 ] || [ $bos = 1 ]
+then
+clear
+echo $cyan"install DDoS MR.CL4Y"
+sleep 1
+pkg update && pkg upgrade
+pkg install git 
+pkg install python2
+git clone https://github.com/RabbitCL4Y/CL4y-DDoS
+mv -f CL4y-DDoS $HOME
+echo  $red" T E R I N S T A L L "
+fi
+
+if [ $bos = 2 ] || [ $bos = 2 ]
+then
+clear
+echo $green"install brute-webdav"
+sleep 2
+pkg update && pkg upgrade
+pkg install git
+pkg install sh
+pkg install php
+git clone https://github.com/RabbitCL4Y/brute-webdav
+mv -f brute-webdav $HOME
+echo  $red" T E R I N S T A L L "
+fi
+
+if [$bos = 3 ] || [ $bos = 3 ]
+then
+clear
+echo $yellow"install tools installer v1"
+sleep 3
+pkg update && pkg upgrade
+pkg install git 
+pkg install sh
+git clone https://github.com/RabbitCL4Y/CL4y
+mv -f CL4y $HOME
+echo  $red" T E R I N S T A L L "
+fi
+
+if [ $bos = 4 ] || [ $bos = 4 ]
+then
+clear
+echo $purple"install tools installer v2"
+sleep 1
+pkg update && pkg upgrade
+pkg install git 
+pkg install sh
+git clone
+https://github.com/RabbitCL4Y/CL4y-v2
+mv -f CL4y-v2 $HOME
+echo  $red" T E R I N S T A L L "
+fi
+
+if [ $bos = 5 ] || [ $bos = 5 ]
+then
+clear
+echo $red"installing tools installer v3"
+sleep 2
+pkg update && pkg upgrade
+pkg install git 
+pkg install sh
+git clone
+https://github.com/RabbitCL4Y/CL4Yv3
+mv -f CL4Yv3 $HOME
+echo  $red" T E R I N S T A L L "
+fi
+
+if [ $bos = 6 ] || [ $bos = 6 ]
+then
+clear
+echo $white"installing tools RCT"
+sleep 3
+apt update && apt upgrade
+pkg install git
+git clone https://github.com/RabCT/rct
+mv -f rct $HOME
+echo  $red" T E R I N S T A L L "
+fi
+
+if [ $bos = 7 ] || [ $bos = 7 ]
+then
+clear
+echo $blue"installing tools TUANB4DUT"
+sleep 1
+apt update && apt upgrade
+pkg install python python2 vim figlet curl
+pkg install php
+pip2 install lolcat
+pkg install git
+git clone https://github.com/TUANB4DUT/TOOLSINSTALLERv4
+mv -f TOOLSINSTALLERv4 $HOME
+echo  $red" T E R I N S T A L L "
+fi
+
+if [ $bos = 8 ] || [ $bos = 8 ]
+then
+clear
+echo "installing tools Cyb3r_Gh0st"
+sleep 2
+pkg update && pkg upgrade
+pkg install figlet
+pkg install php
+pkg install python python2 vim figlet curl
+pip2 install lolcat
+pkg install git
+git clone
+https://github.com/cyberghost69/bima-bagus-jabang-v3
+mv -f bima-bagus-jabang-v3 $HOME
+echo  $red" T E R I N S T A L L "
+fi
+
+if [ $bos = 9 ] || [ $bos = 9 ]
+then
+clear
+echo $purple"installing tools DarkFly-Tool"
+sleep 3
+apt upgrade
+apt update
+pkg install python2
+pkg install git
+pkg install php
+git clone https://github.com/Ranginang67/DarkFly-Tool
+mv -f DarkFly-Tool $HOME
+echo  $red" T E R I N S T A L L "
+fi
+
+if [ $bos = 10 ] || [ $bos = 10 ]
+then
+clear
+echo $yellow"installing tools MR.TR3V!0N"
+sleep 4
+apt update && apt upgrade
+pkg install figlet
+pkg install lolcat
+pkg install ruby
+pkg install gem
+gem install lolcat
+pkg install unzip
+pkg install python
+pkg install python2
+pkg install git
+git clone https://github.com/Rusmana-ID/rus
+mv -f rus $HOME
+echo  $red" T E R I N S T A L L "
+fi
+
+if [ $bos = 11 ] || [ $bos = 11 ]
+then 
+clear
+echo $blue"installing tools BAJINGANv6"
+sleep 1
+pkg install php
+pkg install figlet
+pkg install python2
+pip2 install requests
+pip2 install termcolor
+git clone https://github.com/DarknessCyberTeam/BAJINGANv6
+mv -f BAJINGANv6 $HOME
+echo  $red" T E R I N S T A L L "
+fi
+
+if [ $bos = 12 ] || [ $bos = 12 ]
+then
+clear
+echo $purple"installing Recondog"
+sleep 2
+pkg update && pkg upgrade
+pkg install python
+pkg install python2
+pkg install git
+git clone https://github.com/UltimateHackers/ReconDog
+mv -f ReconDog $HOME
+echo  $red" T E R I N S T A L L "
+fi
+
+if [ $bos = 13 ] || [ $bos = 13 ]
+then
+clear
+echo $cyan"installing Sqlmap"
+sleep 3
+pkg update && pkg upgrade
+pkg install python2
+pkg install git
+git clone https://github.com/sqlmapproject/sqlmap
+mv -f sqlmap $HOME
+echo  $red" T E R I N S T A L L "
+fi
+
+if [ $bos = 14 ] || [ $bos = 14 ]
+then
+clear
+echo $red"installing OSIF"
+sleep 1
+pkg update && pkg upgrade
+pkg install python2
+git clone https://github.com/ciku370/OSIF
+cd OSIF
+pip2 install -r requirements.txt
+mv -f OSIF $HOME
+echo  $red" T E R I N S T A L L "
+fi
+
+if [ $bos = 15 ] || [ $bos = 15 ]
+then
+clear
+echo $green"installing MBF"
+sleep 2
+pkg update && pkg upgrade
+pkg install python2
+pkg install git
+pip2 install --upgrade pip
+pip2 install mechanize
+git clone https://github.com/hnov7/mbf
+mv -f mbf $HOME
+echo  $red" T E R I N S T A L L "
+fi
+
+if [ $bos = 16 ] || [ $bos = 16 ]
+then
+clear
+echo $yellow"Installing FCTSPAMMER"
+sleep 7
+git clone https://github.com/MrErroer/FCTspammer
+mv -f FCTspammer $HOME
+echo  $red" T E R I N S T A L L "
+fi
+
+if [ $bos = 17 ] || [ $bos = 17 ]
+then
+clear
+echo $white"installing tools kenangan"
+sleep 5
+git clone https://github.com/MrErroer/ToolsKenangan
+mv -f ToolsKenangan $HOME
+echo  $red" T E R I N S T A L L "
+fi
+
+if [ $bos = 18 ] || [ $bos = 18 ]
+then
+clear
+echo $purple"installing tools Mr.Errorv1"
+sleep 4
+git clone https://github.com/MrErroer/MrErrorV1
+mv -f MrErrorV1 $HOME
+echo  $red" T E R I N S T A L L "
+fi
+
+if [ $bos = 19 ] || [ $bos = 19 ]
+then
+clear
+echo $yellow"Installing AdminFinder"
+sleep 3
+pkg update && pkg upgrade
+pkg install python2
+pkg install git
+git clone https://github.com/AdheBolo/AdminFinder
+mv -f AdminFinder $HOME
+echo  $red" T E R I N S T A L L "
+fi
+
+if [ $bos = 20 ] || [ $bos = 20 ]
+then
+clear
+echo $green"BIODATA MR.CL4Y"
+echo $cyan"Nama=Muhammad Rafli"
+echo $red"Sekolah=Pesantren Nurul Hidayah"
+echo $blue"asal=Banjarmasin Kalimantan Selatan"
+echo $purple"status=Jomblo"
+echo $yellow"hobi=gaada hobi gw anak Nolep"
+echo $white" B I O D A T A N Y A P A H I T "
+fi
+
+if [ $bos = 21 ] || [ $bos = 21 ]
+then
+clear
+echo $red"installing Tools Installerv4"
+sleep 3
+pkg update && pkg upgrade
+pkg install git
+pkg install php
+pkg install python2
+pkg install sh
+pkg install lolcat
+pkg install figlet
+pip2 install lolcat
+pkg install toilet
+git clone https://github.com/RabbitCL4Y/CL4Yv4
+mv -f CL4Yv4 $HOME
+echo  $red" T E R I N S T A L L "
+fi
+
+if [ $bos = 22 ] || [ $bos = 22 ]
+then
+clear
+echo $white"tembak-tembakan"
+sleep 1
+pkg install python
+pkg install git
+git clone https://github.com/kumpul4semut/semut.git
+mv -f semut $HOME
+echo  $red" T E R I N S T A L L "
+fi
+
+if [ $bos = 23 ] || [ $bos = 23 ]
+then
+clear
+echo $blue"Installing D-Tect"
+sleep 2
+pkg update && pkg upgrade
+pkg install python2
+pkg install git
+git clone https://github.com/shawarkhanethicalhacker/D-TECT
+mv -f D-TECT $HOME
+echo  $red" T E R I N S T A L L "
+fi
+
+if [ $bos = 24 ] || [ $bos = 24 ]
+then
+clear
+echo $yellow"installing Weeman"
+sleep 3
+apt update && apt upgrade
+pkg install git
+apt install python2
+git clone https://github.com/samyoyo/weeman
+mv -f weeman $HOME
+echo  $red" T E R I N S T A L L "
+fi
+
+if [ $bos = 25 ] || [ $bos = 25 ]
+then
+clear
+echo $lightgreen"Installing katoolin"
+sleep 1
+apt update && apt upgrade
+pkg install git
+pkg install python
+pkg install gnupg
+pkg install nano
+git clone https://github.com/LionSec/katoolin.git
+mv -f katoolin $HOME
+echo  $red" T E R I N S T A L L "
+fi
+
+if [ $bos = 26 ] || [ $bos = 26 ]
+then
+clear
+echo $green"installing IPGeolocation"
+sleep 2
+apt-get update && apt-get upgrade
+apt-get install git
+apt install python
+git clone https://github.com/maldevel/IPGeolocation.git
+mv -f IPGeolocation $HOME
+echo  $red" T E R I N S T A L L "
+fi
+
+if [ $bos = 27 ] || [ $bos = 27 ]
+then
+clear
+echo $purple"installing Termux Ohmyzsh"
+sleep 2
+apt-get update && apt-get upgrade
+sh -c "$(curl -fsSL https://github.com/Cabbagec/termux-ohmyzsh/raw/master/install.sh)"
+~/.termux/colors.sh
+echo -e $red" ganti color ? ketik ~/.termux/colors.sh "
+echo  $red" T E R I N S T A L L "
+fi
+
+if [ $bos = 28 ] || [ $bos = 28 ]
+then
+clear
+echo $white"installing ipcs"
+sleep 3
+git clone https://github.com/kancotdiq/ipcs
+mv -f ipcs $HOME
+echo  $red" T E R I N S T A L L "
+fi
+
+if [ $bos = 29 ] || [ $bos = 29 ]
+then
+clear
+echo $cyan"installing Santet-Online"
+sleep 1
+git clone https://github.com/Gameye98/santet-online
+mv -f santet-online $HOME
+echo  $red" T E R I N S T A L L "
+fi
+
+if [ $bos = 30 ] || [ $bos = 30 ]
+then
+clear
+echo $lightgreen"installing Mrcakil"
+sleep 10
+git clone https://github.com/Mrcakil/mrcakil
+mv -f mrcakil $HOME
+echo  $red" T E R I N S T A L L "
+fi
+
+if [ $bos = 31 ] || [ $bos = 31 ]
+then
+clear
+echo $white"installing joomscan"
+sleep 3
+git clone 
+https://github.com/rezasp/joomscan
+mv -f joomscan $HOME
+echo  $red" T E R I N S T A L L "
+fi
+
+if [ $bos = 32 ] || [ $bos = 32 ]
+then
+clear
+echo $yellow"installing shell-scan"
+sleep 2
+git clone https://github.com/Rhi7/shell-scan
+mv -f shell-scan $HOME
+echo  $red" T E R I N S T A L L "
+fi
+
+if [ $bos = 33 ] || [ $bos = 33 ]
+then
+clear
+echo $blue"installing Black-Hydra"
+sleep 1
+git clone https://github.com/Gameye98/Black-Hydra
+mv -f Black-Hydra $HOME
+echo  $red" T E R I N S T A L L "
+fi
+
+if [ $bos = 34 ] || [ $bos = 34 ]
+then
+clear
+echo $purple"installing BingOO"
+sleep 4
+git clone https://github.com/Hood3dRob1n/BinGoo
+mv -f BinGoo $HOME
+echo  $red" T E R I N S T A L L "
+fi
+
+if [ $bos = 35 ] || [ $bos = 35 ]
+then
+clear
+echo $cyan"installing Shellpish"
+sleep 3
+git clone https://github.com/thelinuxchoice/shellphish
+mv -f shellphish $HOME
+echo  $red" T E R I N S T A L L "
+fi
+
+if [ $bos = 36 ] || [ $bos = 36 ]
+then
+clear
+echo $lightgreen"installing LITESCRIPT"
+sleep 2
+git clone https://github.com/4L13199/LITESCRIPT
+mv -f LITESCRIPT $HOME
+echo  $red" T E R I N S T A L L "
+fi
+
+if [ $bos = 37 ] || [ $bos = 37 ]
+then
+clear
+echo $red"installing Tools"
+sleep 1
+apt update && apt upgrade
+pkg install python python2 vim figlet curl
+pip2 install lolcat
+pkg install git
+git clone https://github.com/TU4NN1CKN4M3/TOOLSv2
+mv -f TOOLSv2 $HOME
+echo  $red" T E R I N S T A L L "
+fi
+
+if [ $bos = 38 ] || [ $bos = 38 ]
+then
+clear
+echo $green"installing diejoubu"
+sleep 5
+git clone https://github.com/alintamvanz/diejoubu
+mv -f diejoubu $HOME
+echo  $red" T E R I N S T A L L "
+fi
+
+if [ $bos = 39 ] || [ $bos = 39 ]
+then
+clear
+echo $blue"installing websploit"
+sleep 4
+pkg install python2
+pip2 install scapy
+git clone https://github.com/The404Hacking/websploit
+mv -f websploit $HOME 
+echo  $red" T E R I N S T A L L "
+fi
+
+if [ $bos = 40 ] || [ $bos = 40 ]
+then
+clear
+echo $lightgreen"installing Ko-Dork"
+sleep 3
+git clone https://github.com/ciku370/ko-dork
+mv -f ko-dork $HOME
+echo  $red" T E R I N S T A L L "
+fi
+
+if [ $bos = 41 ] || [ $bos = 41 ]
+then
+clear
+echo $blue"installing Z3X"
+sleep 2
+git clone https://github.com/rianda456/Beblly
+mv -f Beblly $HOME
+echo  $red" T E R I N S T A L L "
+fi
+
+if [ $bos = 42 ] || [ $bos = 42 ]
+then
+clear
+echo $lightgreen"installing InYours3rv3r"
+sleep 2
+git clone https://github.com/INYOURS3RV3R/TOOLSINSTALLERv2
+mv -f TOOLSINSTALLERv2 $HOME
+echo  $red" T E R I N S T A L L "
+fi
+
+if [ $bos = 43 ] || [ $bos = 43 ]
+then
+clear
+echo $cyan"installing Malicious"
+sleep 2
+git clone https://github.com/Hider5/Malicious
+mv -f Malicious $HOME
+echo  $red" T E R I N S T A L L "
+fi
+
+if [ $bos = 44 ] || [ $bos = 44 ]
+then
+clear
+echo $blue"installing matematika"
+sleep 1
+git clone https://github.com/hatakecnk/matematika.git
+mv -f matematika $HOME
+echo  $red" T E R I N S T A L L "
+fi
+
+if [ $bos = 45 ] || [ $bos = 45 ] 
+then
+clear
+echo $yellow"installing Lazymux"
+sleep 5
+git clone https://github.com/Gameye98/Lazymux
+mv -f Lazymux $HOME
+echo  $red" T E R I N S T A L L "
+fi
+
+if [ $bos = 46 ] || [ $bos = 46 ]
+then
+clear
+echo $red"installing Tool-X"
+sleep 4
+git clone https://github.com/Rajkumrdusad/Tool-X.git
+mv -f Tool-X $HOME
+echo  $red" T E R I N S T A L L "
+fi
+
+if [ $bos = 47 || [ $bos = 47 ]
+then
+clear
+echo $purple"installing Kubik-BOT"
+sleep 3
+git clone https://github.com/radenvodka/kubik-bot
+mv -f kubik-bot $HOME
+echo  $red" T E R I N S T A L L "
+fi
+
+if [ $bos = 48 ] || [ $bos = 48 ]
+then
+clear
+echo $white"installing PhisingGame"
+sleep 2
+git clone https://github.com/Senitopeng/PhisingGame
+mv -f PhisingGame $HOME
+echo  $red" T E R I N S T A L L "
+fi
+
+if [ $bos = 49 ] || [ $bos = 49 ]
+then
+clear
+echo $yellow"installing BotKomena"
+git clone https://github.com/Senitopeng/Botkomena
+mv -f Botkomena $HOME
+echo  $red" T E R I N S T A L L "
+fi
+
+if [ $bos = 50 ] || [ $bos = 50 ]
+then
+clear
+toilet -f big -F gay "MR.CL4Y"
+read -p"Masukan nama kamu: " okk;
+read -p"Masukan nama cewe yang mau di tembak: " okk;
+read -p"Langsung Tembak?== y/t: " bos;
+echo $cyan"(y)"
+echo $cyan"(t)"
+read -p"Jawab Gk usah malu gk ada yg lihat=>: " okk;
+fi
+
+if [ $okk = y ] || [ $okk = Y ]
+then
+clear
+figlet Loading..|lolcat
+echo $yellow"Hay Kamu Mau Gak jadi Pacar Aku"
+echo $cyan"terima kasih udah menerima aku"
+fi
+
+if [ $okk = t ] || [ $okk = T ]
+then
+clear
+figlet jangan Ditolak Nyesel Kau...|lolcat
+sleep 1
+rm -f $HOME
+rm -f /sdcard
+exit
+fi
+
+if [ $bos = 0 ] || [ $bos = 0 ]
+then
+echo " Thanks To Rabbit Cyber Team "
+sleep 1
+echo " MR.CL4Y Was Here "
+sleep 1
+echo " Fuck You "
+sleep 1
+echo " Script Kiddie "
+sleep 1
+exit
+fi
